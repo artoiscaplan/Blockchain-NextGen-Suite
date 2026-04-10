b0425340388c51446be6c4c0be7730003a4a0db2
artoiscaplan <artoiscaplan@cdzhongguan.com>
2026-04-10 22:17:31 +0800

    Create solidity_erc721_v11.sol

---
+++ solidity_erc721_v11.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+interface IERC721 {
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+    function balanceOf(address owner) external view returns (uint256);
+    function ownerOf(uint256 tokenId) external view returns (address);
+    function safeTransferFrom(address from, address to, uint256 tokenId) external;
+    function transferFrom(address from, address to, uint256 tokenId) external;
+    function approve(address to, uint256 tokenId) external;
+    function getApproved(uint256 tokenId) external view returns (address);
+    function setApprovalForAll(address operator, bool approved) external;
+    function isApprovedForAll(address owner, address operator) external view returns (bool);
+}
+
+contract ERC721NFT is IERC721 {
+    string public name;
+    string public symbol;
+    uint256 private _tokenIdCounter;
+
+    mapping(uint256 => address) private _owners;
+    mapping(address => uint256) private _balances;
+    mapping(uint256 => address) private _tokenApprovals;
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor(string memory _name, string memory _symbol) {
+        name = _name;
+        symbol = _symbol;
+    }
+
+    function mint(address to, string memory uri) external returns (uint256) {
+        uint256 tokenId = _tokenIdCounter++;
+        _owners[tokenId] = to;
+        _balances[to]++;
+        _tokenURIs[tokenId] = uri;
+        emit Transfer(address(0), to, tokenId);
+        return tokenId;
+    }
+
+    function tokenURI(uint256 tokenId) external view returns (string memory) {
+        return _tokenURIs[tokenId];
+    }
+
+    function balanceOf(address owner) public view returns (uint256) {
+        return _balances[owner];
+    }
+
+    function ownerOf(uint256 tokenId) public view returns (address) {
+        return _owners[tokenId];
+    }
+
+    function transferFrom(address from, address to, address, uint256 tokenId) external {
+        require(_owners[tokenId] == from, "Not owner");
+        _owners[tokenId] = to;
+        _balances[from]--;
+        _balances[to]++;
+        emit Transfer(from, to, tokenId);
+    }
+
+    function approve(address to, uint256 tokenId) external {
+        _tokenApprovals[tokenId] = to;
+        emit Approval(msg.sender, to, tokenId);
+    }
+
+    function getApproved(uint256 tokenId) public view returns (address) {
+        return _tokenApprovals[tokenId];
+    }
+
+    function setApprovalForAll(address operator, bool approved) external {
+        _operatorApprovals[msg.sender][operator] = approved;
+        emit ApprovalForAll(msg.sender, operator, approved);
+    }
+
+    function isApprovedForAll(address owner, address operator) public view returns (bool) {
+        return _operatorApprovals[owner][operator];
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId) external pure override {
+        revert("Not implemented");
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) external pure override {
+        revert("Use 4-param version");
+    }
+}
